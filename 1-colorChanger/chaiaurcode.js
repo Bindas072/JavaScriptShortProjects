2768b8ca838c514a0f5b4e46586961a92554a1ba
Bindas072 <sudipa.mondal05@gmail.com>
2026-02-10 22:46:19 +0530

    Guess The Number

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,27 @@
+const buttons=document.querySelectorAll('.button');
+const body=document.querySelector("body");
+
+
+buttons.forEach((b)=>{
+    b.addEventListener('click',(e)=>{
+        switch(e.target.id){
+            case 'grey':
+                
+                    body.style.backgroundColor=e.target.id;
+                    break;
+            case 'white':
+                
+                    body.style.backgroundColor=e.target.id;
+                    break;
+            case 'blue':
+                
+                    body.style.backgroundColor=e.target.id;
+                    break;
+            case 'yellow':
+                
+                    body.style.backgroundColor=e.target.id;
+                    break;
+
+        }
+    });
+});
